96ee8d793409eae8d24ee02dbe7c00c746181260
Emir <emiratesbp@gmail.com>
2015-03-06 10:05:21 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="444 N. Main Street  #202"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Glen Ellyn, Illinois 60137";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone= 6308581120;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
